57d57e85cdcde4df921bdd4ffc2aae4759b4ee5b
John Marra <joaopaulocmarra@gmail.com>
2024-02-16 13:29:40 -0300

    Add useRef and useSafeAreaInsets hooks to BottomSheet component

--- components/bottom-sheet.tsx
+++ components/bottom-sheet.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import type { PropsWithChildren } from 'react';
-import { Dimensions, Pressable, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
 	FadeIn,
@@ -13,6 +13,7 @@ import Animated, {
 	withSpring,
 	withTiming
 } from 'react-native-reanimated';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 type BottomSheetProps = PropsWithChildren & {
 	visible: boolean;
@@ -20,11 +21,12 @@ type BottomSheetProps = PropsWithChildren & {
 };
 
 const PressAnimated = Animated.createAnimatedComponent(Pressable);
-const HEIGHT = Dimensions.get('window').height * 0.3;
 const CLAMP = 20;
 
 const BottomSheet = (props: BottomSheetProps) => {
 	const offset = useSharedValue(0);
+	const contentHeight = useRef(0);
+	const insets = useSafeAreaInsets();
 
 	const panGesture = Gesture.Pan()
 		.onChange((event) => {
@@ -33,10 +35,10 @@ const BottomSheet = (props: BottomSheetProps) => {
 			offset.value = offsetDelta > 0 ? offsetDelta : withSpring(clamp);
 		})
 		.onFinalize(() => {
-			if (offset.value < HEIGHT / 3) {
+			if (offset.value < contentHeight.current / 3) {
 				offset.value = withSpring(0);
 			} else {
-				offset.value = withTiming(HEIGHT, {}, () => {
+				offset.value = withTiming(contentHeight.current, {}, () => {
 					runOnJS(props.onClose)();
 				});
 			}
@@ -73,7 +75,10 @@ const BottomSheet = (props: BottomSheetProps) => {
 				<Animated.View
 					entering={SlideInDown.springify().damping(15)}
 					exiting={SlideOutDown}
-					style={[styles.view, translateY]}
+					style={[styles.view, translateY, { paddingBottom: insets.bottom }]}
+					onLayout={(event) => {
+						contentHeight.current = event.nativeEvent.layout.height;
+					}}
 				>
 					{props.children}
 				</Animated.View>
@@ -90,13 +95,13 @@ const styles = StyleSheet.create({
 	},
 	view: {
 		backgroundColor: 'white',
-		height: HEIGHT,
 		width: '100%',
 		position: 'absolute',
 		bottom: -CLAMP * 1.1,
 		zIndex: 1,
 		borderTopLeftRadius: 10,
-		borderTopRightRadius: 10
+		borderTopRightRadius: 10,
+		paddingTop: 16
 	}
 });
 
